9d642bcb3fbe8eb2e52d26b65ade6d79570626ea
Sean Perkins <seanperkins@Seans-MacBook-Pro.local>
2026-01-18 23:46:08 -0800

    initial pong commit
    still need to get paddle collision next

---
+++ pong/sketch.js
@@ -0,0 +1,67 @@
+let x = 400;
+let y = 100;
+let xspeed = 5;
+let yspeed = 2;
+let r = 20;
+
+let paddle;
+
+function setup() {
+  createCanvas(800, 600);
+  ball = new Ball(x, y, 5, 2);
+  paddle = new Paddle();
+}
+
+function draw() {
+  background(0);
+  paddle.update();
+  paddle.display();
+  ball.update();
+  ball.display();
+}
+
+class Ball {
+  constructor(x, y, xspeed, yspeed) {
+    this.x = x;
+    this.y = y;
+    this.xspeed = xspeed;
+    this.yspeed = yspeed;
+    this.radius = 20;
+  }
+
+  update() {
+    this.x += this.xspeed;
+    this.y += this.yspeed;
+    if (this.x > width - r || this.x < r) {
+      this.xspeed = -this.xspeed;
+    }
+    if (this.y > height - r || this.y < r) {
+      this.yspeed = -this.yspeed;
+    }
+  }
+
+  display() {
+    fill(color(255, 255, 255));
+    circle(this.x, this.y, this.radius * 2);
+  }
+}
+
+class Paddle {
+  constructor() {
+    this.x = 0;
+    this.y = 585;
+    this.w = 150; // width
+    this.h = 15 // height
+  }
+
+  update() {
+    this.x = constrain(mouseX, 0, width - this.w);
+  }
+
+  display() {
+    fill(255,255,255);
+    rect(this.x, this.y, this.w, this.h);
+  }
+}
+
+
